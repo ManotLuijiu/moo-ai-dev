7212a5d1e5876789145d7ca69aec0d84f93e4ea9
ManotLuijiu <manot_luijiu@hotmail.com>
2024-06-08 19:33:00 +0700

    Adding tailwindcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,21 @@
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
+import {DM_Sans} from 'next/font/google'
 
+import "./globals.css";
+
+const dm_sans = DM_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-dm-sans'
+})
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${dm_sans.variable}`}>
       <body>{children}</body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
